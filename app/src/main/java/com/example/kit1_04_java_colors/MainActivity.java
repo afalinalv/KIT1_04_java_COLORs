943adeb63e2da6b7335bb2855772db8595edb083
afalinalv <afalinalv@ya.ru>
2018-11-19 22:30:26 +0300

    COLORs example AS 3.3.0-betta4 Gradle 4.10.1 Androidx + java API 28-22

--- app/src/main/java/com/example/kit1_04_java_colors/MainActivity.java
+++ app/src/main/java/com/example/kit1_04_java_colors/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.kit1_04_java_colors;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout mConstraintLayout;
+    private TextView mInfoTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mConstraintLayout = findViewById(R.id.constraintLayout);
+        mInfoTextView =findViewById(R.id.textView);
+
+        Button yellowButton = (Button) findViewById(R.id.buttonYellow);
+        Button greenButton = (Button) findViewById(R.id.buttonGreen);
+        greenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mInfoTextView.setText(getText(R.string.green) + "  Listener ");
+                mConstraintLayout.setBackgroundColor(getResources().getColor(R.color.greenColor));
+            }
+        });
+        // Только для JAVA 8 :
+        yellowButton.setOnClickListener(v -> {
+            mInfoTextView.setText(getText(R.string.yellow) + " Lamba ");
+            mConstraintLayout.setBackgroundColor(getResources().getColor(R.color.yellowColor));
+        });
+
+
+    }
+
+    public void onRedButtonClick(View view) {
+        mInfoTextView.setText(getText(R.string.red) + " CLICK ");
+        mConstraintLayout.setBackgroundColor(getResources().getColor(R.color.redColor));
+    }
+
+    public void onYellowButtonClick(View view) {
+        // mInfoTextView.setText(R.string.yellow + " CLICK ");
+        // mConstraintLayout.setBackgroundColor(getResources().getColor(R.color.yellowColor));
+    }
+
+    public void onGreenButtonClick(View view) {
+        // mInfoTextView.setText(R.string.green + " CLICK ");
+        // mConstraintLayout.setBackgroundColor(getResources().getColor(R.color.greenColor));
     }
 }
